28c78e7ab4d8c22426d5e6ec402ebe1fe5c394de
Rabbi Khan <Krabby2244@gmail.com>
2025-12-08 21:34:50 +0600

    navbar almost done

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Components/Shared/Navbar/Navbar';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    );
+};
+
+export default MainLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,18 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import MainLayout from './Layouts/MainLayout.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <MainLayout/>,
+    children: [
+      {
+        path: '/',
+        
+      },
+    ]
   },
 ]);
 
